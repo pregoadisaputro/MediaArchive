856024a669bf4232c01a563f4defba81c4c0b3e8
anlucys <pregoadisaputroo@gmail.com>
2026-06-21 01:40:01 +0700

    feat: make save and load json file service
    - serialize and deserialize json file

---
+++ src/MyMediaArchive/Media/Service/JsonService.cs
@@ -0,0 +1,51 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MyMediaArchive.Media.Service;
+
+public static class JsonService
+{
+    private static readonly JsonSerializerOptions _options = new()
+    {
+        WriteIndented = true,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) },
+    };
+
+    public static void SaveData<T>(T data, string filePath)
+    {
+        try
+        {
+            if (data == null || string.IsNullOrWhiteSpace(filePath))
+            {
+                return;
+            }
+
+            var jsonString = JsonSerializer.Serialize(data, _options);
+            File.WriteAllText(filePath, jsonString);
+        }
+        catch (Exception ex)
+        {
+            throw new IOException($"cannot save the data: {ex}");
+        }
+    }
+
+    public static T? LoadData<T>(string filePath)
+        where T : new()
+    {
+        try
+        {
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                return new T();
+            }
+
+            var jsonString = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<T>(jsonString, _options) ?? new T();
+        }
+        catch (Exception ex)
+        {
+            throw new IOException($"cannot load the data: {ex}");
+        }
+    }
+}
